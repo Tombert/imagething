2f0afdda4c59151020c28f9ca4bf276d015c530b
Thomas Gebert <thomasgebert@gmail.com>
2015-10-15 00:51:53 -0400

    It almost works.  Having trouble getting compilation to happen.

--- backend/src/img_sup.erl
+++ backend/src/img_sup.erl
@@ -5,8 +5,8 @@
 %% API
 -export([start_link/0]).
 
-%% Supervisor callbacks
--export([init/1]).
+
+
 
 %% Helper macro for declaring children of supervisor
 -define(CHILD(I, Type), {I, {I, start_link, []}, permanent, 5000, Type, [I]}).
@@ -16,7 +16,17 @@
 %% ===================================================================
 
 start_link() ->
-    supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+    {ok, Pid} = supervisor:start_link({local, ?MODULE}, ?MODULE, []),
+
+    Dispatch = cowboy_router:compile([
+        {'_', [{"/", hello_handler, [Pid]}]}
+    ]),
+
+%    cowboy:start_http(my_http_listener, 100, [{port, 8080}], 
+%        [{env, [{dispatch, Dispatch}]}]
+%    ),
+
+    {ok, Pid}.
 
 %% ===================================================================
 %% Supervisor callbacks
