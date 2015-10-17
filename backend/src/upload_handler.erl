3a50e53159d5e08b006aefc611992c1f94c6a7e4
Thomas Gebert <thomasgebert@gmail.com>
2015-10-16 23:54:58 -0400

    Get multipart working.  REALLY ugly right now, but works : D

--- backend/src/upload_handler.erl
+++ backend/src/upload_handler.erl
@@ -20,7 +20,7 @@ dispatch(Req, RiakPid) ->
 
 handle(Req,RiakPid) ->
     <<"POST">> = cowboy_req:method(Req),
-    {ok, Body, Req2} = cowboy_req:body(Req),
+    io:format("Body ~p", [acc_multipart(Req,[])]),
     Req3 = cowboy_req:reply(200, [
       {<<"content-type">>, <<"text/plain">>}
     ], "Hello world!", Req),
@@ -32,3 +32,33 @@ handle(Req,RiakPid) ->
 terminate(_Reason, _Req, _State) ->
     
         ok.
+
+acc_multipart(Req, Acc, Riak) ->
+    case cowboy_req:part(Req) of
+        {ok, Headers, Req2} ->
+            [Req4, Body] = case cow_multipart:form_data(Headers) of
+                               {data, _FieldName} ->
+                                   {ok, MyBody, Req3} = cowboy_req:part_body(Req2),
+                                   [Req3, MyBody];
+                               {file, _FieldName, Filename, CType, _CTransferEncoding} ->
+                                   {ok, IoDevice} = file:open( erlang:iolist_to_binary([<<"tmp/">>,Filename]), [raw, write, binary]),
+                                   Req5 = stream_file(Req2, IoDevice, Riak),
+                                   file:close(IoDevice),
+                                   [Req5, <<"skip printing file content">>]
+                           end,
+            acc_multipart(Req4, [{Headers, Body}|Acc]);
+        {done, Req2} ->
+            {lists:reverse(Acc), Req2}
+    end.
+
+stream_file(Req, IoDevice, Riak) ->
+    case cowboy_req:part_body(Req) of
+        {ok, Body, Req2} ->
+            io:format("part_body ok~n", []),
+            file:write(IoDevice, Body),
+            Req2;
+        {more, Body, Req2} ->
+            io:format("part_body more~n", []),
+            file:write(IoDevice, Body),
+            stream_file(Req2, IoDevice)
+    end.
